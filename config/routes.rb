e4a7e6c00b375e387902a094935be55e42f9ef21
Benoit Michelin <benoit.michelin@gmail.com>
2019-05-23 16:34:10 +0200

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :worksites do
+    resources :bookings, only: [ :index]
+  end
+
+  resources :bookings, only: [ :index]
+
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
