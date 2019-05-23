6143523b22d91ee8f2f964e8bf44c6ad9a063e82
Antoine Rambert <antoine.rambert@gmail.com>
2019-05-23 18:46:25 +0200

    debug cloudinary + vue index + seeds

--- app/models/worksite.rb
+++ app/models/worksite.rb
@@ -5,6 +5,4 @@ class Worksite < ApplicationRecord
   validates :address, presence: true
   validates :name, presence: true
   validates :user, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,27 @@
-"Creating two users"
-User.create(email: "proprio@montagu.fr")
-User.create(email: "tony@worker.net")
+puts "Deleting db"
+if Rails.env.development?
+  Booking.destroy_all
+  Worksite.destroy_all
+  User.destroy_all
+end
 
-"Creating a castle"
+
+puts "Creating two users"
+User.create(email: "proprio@montagu.fr", password: "123456")
+User.create(email: "tony@worker.net", password: "123456")
+
+puts "Creating a castle"
 name = "Chateau de Montagu"
 description = "Un samedi par mois, de 9h à 17h, venez entretenir les ruines du château de Montagu : desherbage, délierrage, nettoyage du site, entretien des vestiges, restauration de mobilier archéologique, taille de pierre."
 address = "Montagu"
+url = "https://upload.wikimedia.org/wikipedia/commons/7/70/Ch%C3%A2teau_de_Montagu_2018.jpg"
 castle = Worksite.new(name: name, description: description, address: address)
-castle.user = User.find(1)
+castle.user = User.find(User.pluck(:id).sample)
+castle.remote_photo_url = url
 castle.save
 
-"Creating a booking"
+puts "Creating a booking"
 booking = Booking.new(start_date: "01/07/2019", end_date: "15/07/2019")
-booking.user = User.find(2)
-booking.Worksite = Worksite.find(1)
+booking.user = User.find(User.pluck(:id).sample)
+booking.worksite = Worksite.find(Worksite.pluck(:id).sample)
+booking.save
