1bc91b22ca2ad5c97598bba24ca2ee4937830164
Benoit Michelin <benoit.michelin@gmail.com>
2019-05-24 15:07:12 +0200

    add authorisation

--- app/controllers/worksites_controller.rb
+++ app/controllers/worksites_controller.rb
@@ -1,23 +1,48 @@
 class WorksitesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show, :index]
   def index
-    @worksites = Worksite.all
+    @worksites = policy_scope(Worksite)
   end
 
   def show
+    @worksite = Worksite.find(params[:id])
+    authorize @worksite
   end
 
   def new
+    @worksite = Worksite.new
+    authorize @worksite
   end
 
   def create
   end
 
   def edit
+    @worksite = Worksite.find(params[:id])
+    authorize @worksite
   end
 
   def update
+    @worksite = Worksite.find(params[:id])
+    authorize @worksite
+    @worksite.update(worksite_params)
+    redirect_to worksite_path
   end
 
   def destroy
+    @worksite = Worksite.find(params[:id])
+    @worksite.destroy
+    authorize @worksite
+    redirect_to worksites_path
   end
+
+private
+
+  def venue_params
+    params.require(:worksite).permit(:name, :address, :description, :user_id, :photo)
+  end
+
 end
+
+
+
--- app/models/worksite.rb
+++ app/models/worksite.rb
@@ -1,7 +1,7 @@
 class Worksite < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :address, presence: true
   validates :name, presence: true
   validates :user, presence: true
