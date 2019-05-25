7f0db666b11cfe24db8197accbc5be3ff9b92ef3
Antoine Rambert <antoine.rambert@gmail.com>
2019-05-25 10:51:31 +0200

    routes et controller bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-
+  before_action :set_worksite, only: [:index, :new, :create]
   def index
     @worksite = Worksite.find(params[:worksite_id])
     @worksites = policy_scope(Booking)
@@ -8,7 +8,7 @@ class BookingsController < ApplicationController
 
   def works
     @bookings = Booking.where(user_id: current_user.id)
-    @bookings = policy_scope(Booking)
+    authorize @bookings
   end
 
   def new
@@ -20,8 +20,9 @@ class BookingsController < ApplicationController
     @booking = Booking.new(bookings_params)
     authorize @booking
     @booking.user = current_user
+    @booking.worksite = @worksite
     if @booking.save
-      redirect_to my_bookings
+      redirect_to my_bookings_path
     else
       render :new
     end
@@ -30,13 +31,16 @@ class BookingsController < ApplicationController
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
-
     redirect_to my_bookings
   end
 
   private
 
   def bookings_params
-    params.require(:booking).permit(:start_date, :end_date, :worksite_id)
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
+  def set_worksite
+    @worksite = Worksite.find(params[:worksite_id])
   end
 end
