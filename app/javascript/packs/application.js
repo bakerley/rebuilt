4779df2b40da11377d7a636e990b7deca8fbebec
Nicolas Chican <nchican@omacinema.com>
2019-05-27 14:24:19 +0200

    mapbox initializing for index & show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,8 @@
 import "bootstrap";
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+
+import { initMapbox } from '../plugins/init_mapbox';
+// import { addMarkersToMap } from '../plugins/init_mapbox';
+
+initMapbox();
+// addMarkersToMap();
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_154459) do
+ActiveRecord::Schema.define(version: 2019_05_27_094753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 2019_05_23_154459) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_worksites_on_user_id"
   end
 
