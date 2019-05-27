b0dcb41143416ac3daea35842ede854b329bf05e
Antoine Rambert <antoine.rambert@gmail.com>
2019-05-27 13:18:41 +0200

    fin du booking controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :worksites do
-    resources :bookings, only: [ :index, :new, :create, :destroy ]
+    resources :bookings, only: [ :index, :new, :create, :destroy, :edit, :update ]
   end
 
   get '/bookings', to: 'bookings#works', as: 'my_bookings'
 
-  # get '/worksites/:worksites_id/bookings/new', to: 'bookings#new', as: 'new_bookings'
-  # post '/bookings', to: 'bookings#create'
-
-
-  delete '/bookings/:id', to: 'bookings#destroy'
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
