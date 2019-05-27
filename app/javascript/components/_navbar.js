67d466651182cdc9133b8dca3106ac39d51905bd
Antoine Rambert <antoine.rambert@gmail.com>
2019-05-27 15:31:15 +0200

    espace alloué sous la navbar pour faire respirer le haut des pages dans js

--- app/javascript/components/_navbar.js
+++ app/javascript/components/_navbar.js
@@ -3,6 +3,9 @@ const transparentNavbar = () => {
     const navbar = document.querySelector(".navbar");
     navbar.classList.remove("fixed-top");
     navbar.classList.remove("bg-primary");
+  } else {
+    const pageContent = document.querySelector("#page-content");
+    pageContent.classList.add("pt-3")
   }
 }
 
