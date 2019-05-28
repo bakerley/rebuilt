7adba779df9e1278fc0004e42461e827deb928fe
Camille Paumier <cam.paumier@gmail.com>
2019-05-28 14:27:56 +0200

    search done on page new

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -5,6 +5,10 @@ const addressInput = document.getElementById('query_localisation');
   if (addressInput) {
     places({ container: addressInput });
   }
+const localisationInput = document.getElementById('worksite_address');
+  if (localisationInput) {
+    places({ container: localisationInput });
+  }
 };
 
 export { initAutocomplete };
